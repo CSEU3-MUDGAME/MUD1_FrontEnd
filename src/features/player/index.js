55ea5815d20730fcc1e9facd1bcd0f7171b9945d
theallegrarr <nurudeen.amedu@gmail.com>
2020-03-02 17:08:39 +0100

    player component finished

--- src/features/player/index.js
+++ src/features/player/index.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React from 'react'
+import { connect } from 'react-redux'
+import walkSprite from '../../assets/chars/gabe/gabe-idle-run.png'
 
-export default function Player(props) {
+function Player(props) {
   return(
-    <div>
-      Awesome Game
+    <div
+     style={{
+       position: 'relative',
+       top: props.position[1],
+       left: props.position[0],
+       backgroundImage: `url('${walkSprite}')`,
+       backgroundPosition: '0 0',
+       width: '23px',
+       height: '25px'
+     }}>
+      
     </div>
   )
 }
+
+function mapStateToProps(state){
+  return {
+    ...state.player,
+  }
+}
+
+export default connect(mapStateToProps)(Player)
